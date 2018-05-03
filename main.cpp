5e41b014044f5d7ea521a488e46b8911ec5e27de
szymon <szymonk211@gmail.com>
2018-05-03 16:08:19 +0200

    asf

--- main.cpp
+++ main.cpp
@@ -4,42 +4,41 @@
 
 int b = 5;
 
-//TODO: shortcuts - wyświetlane - tworzenie pliku ze skrótami
-//TODO: todo Todo!
-//proste Settings
-//FIX_ME FIXME fixme
 // multiple screens?
-//new class 
-// shift-alt-arrow lub ctrl-w
+// auto pragma once
+// tooltips bez type:
+// cała funkcja w tooltips
 // goto file
-//
 // automatyczne wypełnianie CmakeLists przy tworzeniu klasy?
-// brak pragma once lub Guard - powinien być warning
-// save all shortcut ?
-// save all przed refactoring? (opcjonalnie)
-// TODO function args tooltip
-// debbug?
-// new source file
-// new source folder
+// rename CMakeList sync
+// extra windows
+// auto - tooltip
 // toggle source/header
-// rename not updating CmakeLists
-
-// help where are key shortcuts
-//
+// funkcja - podpowiedzi - usprawnij
+// improve context menu  ?
+// search everywhere
+// auto indent on paste
+// refactor: extract local
+// refactor: extract function
 
 template <typename T>
 void printStuff(const T & stuff) {
   std::cout << stuff << std::endl;
 }
+int x = 5;
 
 int main() {
   std::cout << "Hello World!\n";
   b = 10;
-  using namespace std;
-  vector<int> test = {1, 1, 1};
-  auto a = 5;
-  auto val = 7;
- auto val2 = & val;
-  cout << std::accumulate(test.begin(), test.end(), 0, [](auto l, auto r) {return l + r;}) << endl;
   
+  using namespace std;
+  
+  vector<int> test(1000, 1);
+  vector<vector<int>> test2(2, test);
+  x = 10;
+  
+  auto val = 7;
+  auto val2 = & val;
+  
+  cout << std::accumulate(test.begin(), test.end(), 1, std::multiplies<int>()) << endl;  
 }
